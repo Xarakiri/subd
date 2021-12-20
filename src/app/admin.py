f8b012659f1b265e1456a8cb7131a70074e8647d
Xarakiri <rafikov.ds777@gmail.com>
2021-12-20 18:47:51 +0500

    register models

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Teacher, Disciplines, Faculties, TeacherDiscipline
 
-# Register your models here.
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'surname', 'patronomic', 'academic_rank']
+
+
+@admin.register(Disciplines)
+class DisciplinesAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Faculties)
+class FacultiesAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'teacher_id']
+
+
+@admin.register(TeacherDiscipline)
+class TeacherDisciplineAdmin(admin.ModelAdmin):
+    search_fields = ['teacher_id', 'discipline_id']
